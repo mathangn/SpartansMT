1f0437839dc0718bd237e8fb326b57e84b6497de
Mathankumar G N <kumarmadhan432@gmail.com>
2023-06-18 00:37:02 +0530

    updated version

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Bugs](https://sonarcloud.io/api/project_badges/measure?project=fssa-batch3_mathankumar.nagarajan__web_project&metric=bugs)](https://sonarcloud.io/summary/new_code?id=fssa-batch3_mathankumar.nagarajan__web_project)
 
 # SpartansMT
 About your project:
@@ -7,12 +8,214 @@ About your project:
     to buy items through my website. 
 
 
-Problem statement: https://docs.google.com/document/d/1YLwa8yKuOSpcxANGFA6FnlKSpnsf2mJPSzrixnADBbg/edit?usp=sharing
+- Problem statement: https://docs.google.com/document/d/1YLwa8yKuOSpcxANGFA6FnlKSpnsf2mJPSzrixnADBbg/edit?usp=sharing
 
-Market Research: https://docs.google.com/document/d/1Vxxc2iwXF0qa3UHIpjDakZqRnJoUpr6omjUhbAZTNqU/edit?usp=sharing
+- Market Research: https://docs.google.com/document/d/1Vxxc2iwXF0qa3UHIpjDakZqRnJoUpr6omjUhbAZTNqU/edit?usp=sharing
 
-User flow: https://drive.google.com/file/d/1VQ4HswgLBieRTho8TE_mZ4xMFhESaHq_/view?usp=sharing
+- User flow: https://drive.google.com/file/d/1VQ4HswgLBieRTho8TE_mZ4xMFhESaHq_/view?usp=sharing
 
-Wireframe: https://drive.google.com/file/d/1alBJHaJyMYsVhkzoeCH8u0WPmCKyAfGc/view?usp=sharing
+- Wireframe: https://drive.google.com/file/d/1alBJHaJyMYsVhkzoeCH8u0WPmCKyAfGc/view?usp=sharing
 
-pages link:https://mathangn.github.io/SpartansMT/
+- Pages Link : https://fssa-batch3.github.io/mathankumar.nagarajan__web_project/
+
+
+<h1>Features</h1>
+
+<h2>Feature Status</h2>
+
+- Register (Completed)
+- Login (Completed)
+- Profile CRUD (Completed)
+- Store CURD (Completed)
+- Product CURD (Completed)
+- Search Box (Yet to Start)
+- Cart CURD (Yet to Start)
+- Add Payment Details (Yet to Start)
+- Add Order (Yet to Start)
+- Cancel Order (Yet to Start)
+
+
+
+<h2>Buyer</h2>
+
+<h3>Create an account (completed)</h3>
+
+Scenario 1: Successfully create an account
+Steps:
+1. Navigate to the registration page.
+2. Enter the required information such as first_name, last_name, address, email, create_password and confirm_password.
+3. Click the "Create Account" button.
+> Expected Result:
+> The user is redirected to the login page.
+
+
+<h3>Login account (completed)</h3>
+
+Scenario 1: Successfully login in to account
+Steps:
+1. Navigate to the login page.
+2. Enter the required information such as email, and password.
+3. Click the "login" button.
+> Expected Result:
+> The user is redirected to the user profile page.
+
+
+<h3>View account details (completed)</h3>
+
+Scenario 1: Successfully view account details
+Steps:
+1. Navigate to the account page.
+2. View the account details such as first name, last name, email, and address.
+> Expected Result:
+> The user can view their account details.
+
+
+<h3>Edit account details (completed)</h3>
+
+Scenario 1: Successfully edit account details
+Steps:
+1. In the Profile page.
+2. Click the "Edit" button to Edit Your details.
+3. And fill extra details Like Address 2, zipcode, country and state.
+> Expected Result:
+> The user account details has edit but its not updated in the localstorage.
+
+
+<h3>Update account details (completed)</h3>
+
+Scenario 1: Successfully Update account details
+Steps:
+1. In the Profile page.
+2. You already Changed details.
+3. So, Click the "Update" button to Update Your details in localstorage.
+> Expected Result:
+> The user has updated his detail in the local storage.
+
+
+<h3>Delete account details (completed)</h3>
+
+Scenario 1: Successfully Delete account details
+Steps:
+1. In the Profile page.
+2. So, Click the "Delete" button to Delete Your details in localstorage.
+> Expected Result:
+> The user has Deleted his detail in the local storage.
+
+
+
+<h2>Seller</h2>
+
+
+<h3>Create a new store (completed)</h3>
+
+Scenario 1: Successfully created a new product to sell our product.
+Steps:
+1. Go to the home page and log out of your account.
+2. it's going to an index page.
+3. In the page go to the footer and click on the "Home" word in the footer it's open the form.
+4. Enter the store details such as name, category and give unique id as a number.
+5. Upload store logo url link in the "Store URL" input box.
+6. Click the "Create" button.
+> Expected Result:
+> The new store is created and added to the Localstorage. The Store shows the user home page.
+
+
+
+<h3>Edit store details (completed)</h3>
+
+Scenario 1: Successfully view list of products
+Steps:
+1. in the store form page in the "value" input box enter your store unique id.
+2. To click the "Edit" Button.
+3. To get the Store Information and then change your new updates about the store details 
+2. Match the store id to get the store information.
+> Expected Result:
+> The seller can view their store details in the input boxes.
+
+
+
+<h3>Update store details (completed)</h3>
+
+Scenario 1: Successfully Updated store details.
+Steps:
+1. After changing details Click the "Update" button to update new details.
+2. It's Changed in the local storage from old details to updated details.
+> Expected Result:
+> The seller can Update their store details in the local storage.
+
+
+<h3>Delete store details (completed)</h3>
+
+Scenario 1: Successfully Deleted store details.
+Steps:
+1. In the value input box to put the store unique id.
+2. And Click "Delete" Button.
+> Expected Result:
+> The seller can Delete their store details in the local storage.
+
+
+<h3>Create a new product (completed)</h3>
+
+Scenario 1: Successfully create a new product to sell
+Steps:
+1. Go to index page.
+2. In the index page go to the footer and click on the "Stores" word in the footer it's open the form.
+3. Enter the your store name to add product details like Product Title, Price, Full Price, Discount, Rating, Reviews, and Add Product image into URL.
+4. And give give Unique id the product as "Value".
+4. Click the "Create Product" button.
+> Expected Result:
+> The new product is created and added to the Localstorage. it's shows in the store.
+
+
+<h3>Edit product details (completed)</h3>
+
+Scenario 1: Successfully Edited Product Details.
+Steps:
+1. In the form match the Product id as Value and Store name as a Store name in the form.
+2. Click the "Edit" button.
+2. To get the All details about that product.
+> Expected Result:
+> The seller can view their product details in the input boxes.
+
+
+<h3>Update product details (completed)</h3>
+
+Scenario 1: Successfully Updated product details.
+Steps:
+1. After Clicked 'Edit' button to change some details new.
+2. And then Click "Update" Button.
+> Expected Result:
+> The seller can Updated product details in Localstorage.
+
+
+<h3>Delete product details (completed)</h3>
+
+Scenario 1: Successfully Deleted product details.
+Steps:
+1. In the form Match product_id and match Store Name to get the details
+2. And then Click "Delete Product" Button.
+> Expected Result:
+> The seller can Delete product details in Localstorage.
+
+
+
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,636 @@
+const stores = [
+  {
+    image: "https://iili.io/HWXkdkx.png",
+    title: "Nike",
+    category: "Shoes Shop",
+    value: "0",
+    id: 1681360899277,
+    products: [
+      {
+        image: "https://iili.io/HkeiZmb.webp",
+        title: "Mens Air Zoom Vomero 15 Running Shoe",
+        price: "6000",
+        value: "1",
+        full_price: "10000",
+        discount: "40",
+        rating: "4.5",
+        reviews: "2023",
+      },
+      {
+        image: "https://iili.io/HkeiQku.webp",
+        title: "Men's Ultra Light Running and Training Shoes",
+        price: "2500",
+        value: "2",
+        full_price: "5000",
+        discount: "50",
+        rating: "4.2",
+        reviews: "2023",
+      },
+      {
+        image: "https://iili.io/HkeiDIj.webp",
+        title: "Men's Running Shoe Bacca Bucci",
+        price: "1899",
+        value: "3",
+        full_price: "5000",
+        discount: "52",
+        rating: "4.5",
+        reviews: "1992",
+      },
+      {
+        image: "https://iili.io/HUEGr8b.webp",
+        title: "Mens Slip-on Premium Comfy-fit Sneaker",
+        price: "1399",
+        value: "4",
+        full_price: "1999",
+        discount: "29",
+        rating: "4.5",
+        reviews: "1309",
+      },
+      {
+        image: "https://iili.io/HUEMhf1.webp",
+        title: "Men's Wonder-13 Sports Running Shoe",
+        price: "1799",
+        value: "5",
+        full_price: "2999",
+        discount: "39",
+        rating: "4.2",
+        reviews: "1784",
+      },
+      {
+        image: "https://iili.io/HUEMj0F.webp",
+        title: "Mens Ultimate Ease Range Walking Shoe",
+        price: "1199",
+        value: "6",
+        full_price: "2999",
+        discount: "59",
+        rating: "4.2",
+        reviews: "2031",
+      },
+      {
+        image: "https://iili.io/HUEMWiP.webp",
+        title: "Men's Wonder-13 Sports Running Shoes",
+        price: "1679",
+        value: "7",
+        full_price: "2799",
+        discount: "39",
+        rating: "4.2",
+        reviews: "4564",
+      },
+      {
+        image: "https://iili.io/HUEMVWB.webp",
+        title: "ASIAN Delta-20 Sports Running Walking",
+        price: "2399",
+        value: "8",
+        full_price: "3999",
+        discount: "39",
+        rating: "4.7",
+        reviews: "4660",
+      },
+      {
+        image: "https://iili.io/HUEMwUg.webp",
+        title: "Flash Shoe Badminton Shoes for Mens",
+        price: "1479",
+        value: "9",
+        full_price: "3699",
+        discount: "59",
+        rating: "4.5",
+        reviews: "2370",
+      },
+      {
+        image: "https://iili.io/HUEMkOv.webp",
+        title: "Men's Cosko Sports Running, Walking,Gym",
+        price: "3239",
+        value: "10",
+        full_price: "5399",
+        discount: "40",
+        rating: "4.2",
+        reviews: "7652",
+      },
+      {
+        image: "https://iili.io/HUEMOJa.webp",
+        title: "Crystal-13 Sports Running Walking Shoes",
+        price: "2299",
+        value: "11",
+        full_price: "4999",
+        discount: "54",
+        rating: "4.6",
+        reviews: "4351",
+      },
+      {
+        image: "https://iili.io/HUEMe5J.webp",
+        title: "Skypee-31 Running Shoes,Walking Shoes",
+        price: "1280",
+        value: "12",
+        full_price: "3999",
+        discount: "68",
+        rating: "4.1",
+        reviews: "3428",
+      },
+    ],
+    banner: "https://iili.io/HU1PE0u.jpg",
+  },
+  {
+    image: "https://iili.io/HWXkKhB.jpg",
+    title: "Lenskart",
+    category: "Specse Shop",
+    value: "2",
+    id: 1683014799812,
+    products: [
+      {
+        image: "https://iili.io/HUMfnUu.jpg",
+        title: "Black Sky Full Rim Square Eyeglasses",
+        price: "2700",
+        value: "13",
+        full_price: "3024",
+        discount: "11",
+        rating: "4.5",
+        reviews: "4981",
+      },
+      {
+        image: "https://iili.io/HUMfzRj.jpg",
+        title: "Black Full Rim Square Eyeglasses",
+        price: "1500",
+        value: "14",
+        full_price: "1680",
+        discount: "11",
+        rating: "4.2",
+        reviews: "1723",
+      },
+      {
+        image: "https://iili.io/HUMfCle.jpg",
+        title: "Black Silver Full Rim Round Eyeglasses",
+        price: "1500",
+        value: "15",
+        full_price: "1680",
+        discount: "11",
+        rating: "4.4",
+        reviews: "6592",
+      },
+      {
+        image: "https://iili.io/HUMfxHb.jpg",
+        title: "Black & Green Silver Full Rim Round Eyeglasses",
+        price: "1500",
+        value: "16",
+        full_price: "1680",
+        discount: "11",
+        rating: "4.4",
+        reviews: "5423",
+      },
+      {
+        image: "https://iili.io/HUMfIOx.jpg",
+        title: "Gold Silver Full Rim Round Eyeglasses",
+        price: "1999",
+        value: "17",
+        full_price: "2999",
+        discount: "33",
+        rating: "4.6",
+        reviews: "9657",
+      },
+      {
+        image: "https://iili.io/HUMfTDQ.jpg",
+        title: "Dark Green Silver Full Rim Round Eyeglasses",
+        price: "1500",
+        value: "18",
+        full_price: "1680",
+        discount: "11",
+        rating: "4.1",
+        reviews: "7622",
+      },
+      {
+        image: "https://iili.io/HUMfAxV.jpg",
+        title: "Dark Blue Rim Rectangle Eyeglasses",
+        price: "1500",
+        value: "19",
+        full_price: "1680",
+        discount: "11",
+        rating: "4.7",
+        reviews: "6396",
+      },
+      {
+        image: "https://iili.io/HUMfRWB.jpg",
+        title: "Green Rim Rectangle Eyeglasses",
+        price: "1500",
+        value: "20",
+        full_price: "1680",
+        discount: "11",
+        rating: "4.3",
+        reviews: "6306",
+      },
+      {
+        image: "https://iili.io/HUMf5iP.jpg",
+        title: "Golden Silver Full Rim Round Eyeglasses",
+        price: "1700",
+        value: "21",
+        full_price: "2000",
+        discount: "15",
+        rating: "4.0",
+        reviews: "7553",
+      },
+      {
+        image: "https://iili.io/HUMfYf1.jpg",
+        title: "Black Silver Full Rim Round Eyeglasses",
+        price: "1500",
+        value: "22",
+        full_price: "1680",
+        discount: "11",
+        rating: "4.6",
+        reviews: "4673",
+      },
+      {
+        image: "https://iili.io/HUMfa0F.jpg",
+        title: "Pink Silver Full Rim Round Eyeglasses",
+        price: "1500",
+        value: "23",
+        full_price: "1680",
+        discount: "11",
+        rating: "4.6",
+        reviews: "4673",
+      },
+      {
+        image: "https://iili.io/HUMfcUg.jpg",
+        title: "Black Full Rim Round Eyeglasses",
+        price: "1500",
+        value: "24",
+        full_price: "1680",
+        discount: "11",
+        rating: "4.2",
+        reviews: "9834",
+      },
+    ],
+    banner: "https://iili.io/HU1i1hN.jpg",
+  },
+  {
+    image: "https://iili.io/HWXep1e.png",
+    title: "Chales & Keith",
+    category: "Bags & Luggage",
+    value: "3",
+    id: 1683014822216,
+    products: [
+      {
+        image: "https://iili.io/HUMcGVt.webp",
+        title: "Medium 22 L Laptop Backpack (14 inch) ",
+        price: "1600",
+        value: "25",
+        full_price: "2600",
+        discount: "38",
+        rating: "4.2",
+        reviews: "8734",
+      },
+      {
+        image: "https://iili.io/HUMcExI.webp",
+        title: "Soft Body Set of 2 Luggage",
+        price: "8700",
+        value: "26",
+        full_price: "11300",
+        discount: "23",
+        rating: "4.0",
+        reviews: "1993",
+      },
+      {
+        image: "https://iili.io/HUMcMiX.webp",
+        title: "Medium Check-in Suitcase (68 cm) ",
+        price: "2399",
+        value: "27",
+        full_price: "9775",
+        discount: "75",
+        rating: "4.5",
+        reviews: "8649",
+      },
+      {
+        image: "https://iili.io/HUMc0DN.webp",
+        title: "Medium Check-in Suitcase (67 cm)",
+        price: "3899",
+        value: "28",
+        full_price: "9200",
+        discount: "57",
+        rating: "4.4",
+        reviews: "4803",
+      },
+      {
+        image: "https://iili.io/HUMcWfn.webp",
+        title: "Small Cabin Suitcase (50 cm)",
+        price: "2299",
+        value: "29",
+        full_price: "7140",
+        discount: "67",
+        rating: "4.5",
+        reviews: "2115",
+      },
+      {
+        image: "https://iili.io/HUMcXls.webp",
+        title: "Medium Check-in Suitcase (68 cm)",
+        price: "4000",
+        value: "30",
+        full_price: "6300",
+        discount: "36",
+        rating: "4.2",
+        reviews: "4927",
+      },
+    ],
+    banner: "https://iili.io/HU1ivQS.png",
+  },
+  {
+    image: "https://iili.io/HWXeyru.png",
+    title: "Vero Moda",
+    category: "Fashion Dress For Women",
+    value: "4",
+    id: 1683014846248,
+    products: [
+      {
+        image: "https://iili.io/HUMEIkJ.webp",
+        title: "Length Western Skirt and High Neck T-Shirt Dress",
+        price: "570",
+        value: "37",
+        full_price: "1200",
+        discount: "53",
+        rating: "3.9",
+        reviews: "404",
+      },
+      {
+        image: "https://iili.io/HUMEnrF.webp",
+        title: "PURVAJA Women’s High-Low Knee Length Dress",
+        price: "371",
+        value: "38",
+        full_price: "1999",
+        discount: "87",
+        rating: "4.0",
+        reviews: "508",
+      },
+      {
+        image: "https://iili.io/HUMExdg.webp",
+        title: "Women Ethnic Dress Multicolor Dress",
+        price: "850",
+        value: "39",
+        full_price: "900",
+        discount: "5",
+        rating: "4.5",
+        reviews: "1574",
+      },
+      {
+        image: "https://iili.io/HUMEC11.webp",
+        title: "Women A-line White Dress",
+        price: "499",
+        value: "40",
+        full_price: "1849",
+        discount: "73",
+        rating: "4.0",
+        reviews: "4183",
+      },
+      {
+        image: "https://iili.io/HUMEz7a.webp",
+        title: "Women Empire Waist Black, White, Pink Dress",
+        price: "362",
+        value: "41",
+        full_price: "2699",
+        discount: "86",
+        rating: "4.0",
+        reviews: "2177",
+      },
+      {
+        image: "https://iili.io/HUMETmv.webp",
+        title: "Women Fit and Flare Black Dress",
+        price: "699",
+        value: "42",
+        full_price: "799",
+        discount: "12",
+        rating: "4.2",
+        reviews: "3498",
+      },
+      {
+        image: "https://iili.io/HUMEAIR.webp",
+        title: "Flare Style Neck and Knee Length Dress",
+        price: "492",
+        value: "43",
+        full_price: "1200",
+        discount: "59",
+        rating: "4.2",
+        reviews: "3098",
+      },
+      {
+        image: "https://iili.io/HUMERXp.webp",
+        title: "Women A-line Pink Dress",
+        price: "1299",
+        value: "44",
+        full_price: "1299",
+        discount: "0",
+        rating: "4.3",
+        reviews: "8486",
+      },
+      {
+        image: "https://iili.io/HUME5LN.webp",
+        title: "Women Fit and Flare Dark Blue Dress",
+        price: "403",
+        value: "45",
+        full_price: "1999",
+        discount: "79",
+        rating: "4.2",
+        reviews: "7545",
+      },
+      {
+        image: "https://iili.io/HUMEYBI.webp",
+        title: "Women A-line White Dress",
+        price: "549",
+        value: "46",
+        full_price: "1299",
+        discount: "57",
+        rating: "3.5",
+        reviews: "2947",
+      },
+      {
+        image: "https://iili.io/HUMEa1t.webp",
+        title: "Women A-line Maroon Dress",
+        price: "449",
+        value: "47",
+        full_price: "1299",
+        discount: "65",
+        rating: "3.7",
+        reviews: "4856",
+      },
+      {
+        image: "https://iili.io/HUMEcrX.webp",
+        title: "Women A-line Black Dress",
+        price: "799",
+        value: "48",
+        full_price: "899",
+        discount: "11",
+        rating: "4.5",
+        reviews: "558",
+      },
+    ],
+    banner: "https://iili.io/HU1LfOx.gif",
+  },
+  {
+    image: "https://iili.io/HWXknrg.png",
+    title: "Poorvika",
+    category: "Mobile Shop",
+    value: "5",
+    id: 1683014867397,
+    products: [
+      {
+        image: "https://iili.io/HUMtdvt.webp",
+        title: "Tecno Spark 9 (Infinity Black, 4GB RAM,64GB Storage)",
+        price: "7799",
+        value: "49",
+        full_price: "9899",
+        discount: "26",
+        rating: "3.9",
+        reviews: "1234",
+      },
+      {
+        image: "https://iili.io/HUMtFun.webp",
+        title: "IQOO Z6 44W (Lumina Blue, 128 GB)  (8 GB RAM)",
+        price: "16499",
+        value: "50",
+        full_price: "19999",
+        discount: "15",
+        rating: "4.5",
+        reviews: "7649",
+      },
+    ],
+    banner: "https://iili.io/HU1QB7s.jpg",
+  },
+  {
+    image: "https://iili.io/HWXk2mQ.gif",
+    title: "MAC",
+    category: "Beauty Products",
+    value: "6",
+    id: 1683014885991,
+    products: [],
+    banner: "https://iili.io/HU1tR5u.png",
+  },
+  {
+    image: "https://iili.io/HWXemB9.png",
+    title: "Jack & Zone",
+    category: "Mens Fashion",
+    value: "7",
+    id: 1683014904741,
+    products: [],
+    banner: "https://iili.io/HU1tQB2.png",
+  },
+  {
+    image: "https://iili.io/HWXkJ7j.jpg",
+    title: "Label Ritu Kumar",
+    category: "Designer Wear",
+    value: "8",
+    id: 1683014925930,
+    products: [],
+    banner: "https://iili.io/HU1Dq21.png",
+  },
+  {
+    image: "https://iili.io/HWXkBB1.jpg",
+    title: "ivenus",
+    category: "Apple Premium Reseller",
+    value: "9",
+    id: 1683014948464,
+    products: [
+      {
+        image: "https://iili.io/HSxe88l.webp",
+        title: "Apple iPhone 14 Pro Max",
+        price: "127999",
+        value: "101",
+        full_price: "139999",
+        discount: "9",
+        rating: "4.5",
+        reviews: "2023",
+      },
+      {
+        image: "https://iili.io/HSxeO6G.webp",
+        title: "Apple iPhone 13 (128GB) - (Product)",
+        price: "62999",
+        value: "102",
+        full_price: "69999",
+        discount: "10",
+        rating: "4.5",
+        reviews: "2023",
+      },
+    ],
+    banner: "https://iili.io/HU1DLua.png",
+  },
+  {
+    image: "https://iili.io/HWXkHdb.jpg",
+    title: "SS Beauty Shop",
+    category: "Beauty Shop",
+    value: "1",
+    id: 1683982354957,
+    products: [],
+    banner: "https://iili.io/HU1i72a.webp",
+  },
+];
+
+if (localStorage.getItem("stores")) {
+  console.log("Working");
+} else {
+  localStorage.setItem("stores", JSON.stringify(stores));
+}
+
+// index page script codes
+let div_store;
+let img_imgstore;
+let div_floor;
+let div;
+let h3_storenames;
+let p_storecategory;
+let p_viewmore;
+let a_items;
+
+// const stores = JSON.parse(localStorage.getItem("stores"))
+
+for (let i = 0; i < 10; i++) {
+  // console.log("hello world")
+
+  a_items = document.createElement("a"); // create tage
+  // a_items.setAttribute(
+  //   "href",
+  //   `pages/stores/prducts/nike.html?value=${stores[i].value}`
+  // );
+  a_items.addEventListener("click", function(){
+    let hidden_div = document.getElementById("hidden_div")
+    hidden_div.style.display = "flex"
+  })
+
+  // adding class or same attribute
+
+  // console.log(a_items);                               // view code
+
+  div_store = document.createElement("div");
+  div_store.setAttribute("class", "store");
+  // console.log(div_store)
+  a_items.append(div_store); // placeing place
+
+  img_imgstore = document.createElement("img"); // creating img tag
+  img_imgstore.setAttribute("src", stores[i].image); // add some attribute
+  img_imgstore.setAttribute("alt", "Store Logo");
+  img_imgstore.setAttribute("class", "imgstore");
+  div_store.append(img_imgstore);
+
+  div_floor = document.createElement("div");
+  div_floor.setAttribute("class", "floor");
+  div_store.append(div_floor);
+
+  div = document.createElement("div");
+  div_floor.append(div);
+
+  h3_storenames = document.createElement("h3");
+  h3_storenames.setAttribute("class", "storenames");
+  h3_storenames.innerText = stores[i].title;
+  div.append(h3_storenames);
+
+  p_storecategory = document.createElement("p");
+  p_storecategory.setAttribute("class", "store_category");
+  p_storecategory.innerText = stores[i].category;
+  div.append(p_storecategory);
+
+  p_viewmore = document.createElement("p");
+  p_viewmore.setAttribute("class", "viewmore");
+  p_viewmore.innerText = "View More";
+  div.append(p_viewmore);
+
+  // document.querySelector("div.stores").append(div_store);
+  document.querySelector("div.stores").append(a_items);
+}
+
+if(localStorage.getItem("active_user")){
+  localStorage.removeItem("active_user")
+}else{
+  console.log("already removed")
+}
